a01379499d2850cdf6ab856f4b68cf1a053acf79
Adegbenle1 <56692985+Adegbenle1@users.noreply.github.com>
2019-10-26 18:18:04 +0200

    Add files via upload

--- EvenOdd.java
+++ EvenOdd.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 
-//loops
-
 public class EvenOdd {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int n = scan.nextInt();
-		//todo: The program takes a number as input.
-		//It then goes through all integer values till reaching that value and prints all the odd ones.
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+			if (n%2==1) {
+				System.out.prinln(n);
+			}
+		}
+		
 		
 	}
 	
-}
