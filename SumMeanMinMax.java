b61eb9ef7b094292a0bb38f79978f6c712836bbe
Adegbenle1 <56692985+Adegbenle1@users.noreply.github.com>
2019-10-26 18:19:57 +0200

    Add files via upload

--- SumMeanMinMax.java
+++ SumMeanMinMax.java
@@ -2,19 +2,35 @@ import java.util.Scanner;
 
 
 public class SumMeanMinMax {
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner scan = new Scanner(System.in);
-		int a = scan.nextInt();
-		int b = scan.nextInt();
-		int c = scan.nextInt();
+		
+		Scanner input = new Scanner(System.in);
+		int a = input.nextInt();
+		int b = input.nextInt();
+		int c = input.nextInt();
+		int n = a+b+c;
+		double m = n/3;
+		int max = 0;
+		int min = 0;
+		for (int i=0;i<a;i++) {
+		int y = input.nextInt()
+		if (i==0) {
+		max = y;
+		min = y;
+		}
+		else if (y>max) {
+		max = y;
+		}else if (y<min) {
+		min = y;
+		}
+		else {continue;
+		}
+		System.out.println("the maximum number is "+max);
+		System.out.println("the minimum number is "+min);
+		}
+		System.out.println(n,m);
 		
 		System.out.println("a: " + a + " b: " + b + " c: " + c);
-
 		/*
 		 * Todo: Print the sum, mean, max Value and min Value of a, b and c.
 		 * */
